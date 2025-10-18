616480f046f19c1d08e33dab5e345cc8169b9b86
PeppPercoc <paolo.martino1234@gmail.com>
2025-10-18 15:42:42 +0200

    Created NavBar

---
+++ lib/screens/scheduler_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SchedulerScreen extends StatelessWidget {
+  const SchedulerScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Scheduler'));
+  }
+}
---
+++ lib/screens/vitals_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class VitalsScreen extends StatelessWidget {
+  const VitalsScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Vitals'));
+  }
+}
