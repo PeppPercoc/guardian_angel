9774da5d19015af2c54d9505e3cc330fe9230b46
PeppPercoc <paolo.martino1234@gmail.com>
2025-10-18 18:27:52 +0200

    Update NavBar w/ google_nav_bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:guardian_angel/main.dart';
 import 'home_screen.dart';
 import 'scheduler_screen.dart';
@@ -33,7 +34,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1;
 
   static final List<Widget> _pages = <Widget>[
     SchedulerScreen(),
@@ -49,56 +50,87 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    // Calcola un padding orizzontale dinamico, magari 5% della larghezza per lato
+    double horizontalPadding = screenWidth * 0.05;
+
     return Scaffold(
-  body: Column(
-    children: [
-      GestureDetector(
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const EmergencyScreen()),
-          );
-        },
-        child: Container(
-          color: Colors.red,
-          height: 56,
-          width: double.infinity,
-          alignment: Alignment.center,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.warning_rounded, color: Colors.white),
-              SizedBox(width: 8),
-              Text(
-                'SOS EMERGENCY',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const EmergencyScreen(),
                 ),
+              );
+            },
+            child: Container(
+              color: Colors.red,
+              height: 56,
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.warning_rounded, color: Colors.white),
+                  SizedBox(width: 8),
+                  Text(
+                    'SOS EMERGENCY',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
+            ),
+          ),
+          Expanded(child: _pages[_selectedIndex]),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              blurRadius: 20,
+              offset: const Offset(0, -3),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: horizontalPadding,
+            vertical: 12,
+          ),
+          child: GNav(
+            gap: 8,
+            activeColor: AppColors.secondary,
+            iconSize: 30,
+            padding: EdgeInsets.symmetric(
+              horizontal: horizontalPadding / 2,
+              vertical: 12,
+            ),
+            duration: const Duration(milliseconds: 400),
+            tabBackgroundColor: AppColors.background,
+            color: Colors.grey,
+            tabs: const [
+              GButton(icon: Icons.medication, text: 'Scheduler'),
+              GButton(icon: Icons.home, text: 'Home'),
+              GButton(icon: Icons.favorite, text: 'Vitals'),
             ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
         ),
-      ),
-      Expanded(
-        child: _pages[_selectedIndex],
-      ),
-    ],
-  ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.medication),
-            label: 'Scheduler',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Vitals'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        backgroundColor: Colors.white,
       ),
     );
   }
