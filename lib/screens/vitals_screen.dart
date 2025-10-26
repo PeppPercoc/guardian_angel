9cd60fe7f5406d9925d4ef17a8082f0cf6113ceb
PeppPercoc <paolo.martino1234@gmail.com>
2025-10-27 00:26:19 +0100

    DEBUG: moved reset Shared Preferences in vitals_screen

--- lib/screens/vitals_screen.dart
+++ lib/screens/vitals_screen.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class VitalsScreen extends StatelessWidget {
   const VitalsScreen({super.key});
-  @override
+  Future<void> _resetPrefs(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+
+    if (!context.mounted) return;
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('SharedPreferences resettate con successo!'),
+        backgroundColor: Colors.green,
+      ),
+    );
+    Navigator.of(context).pushNamedAndRemoveUntil('/welcome', (route) => false);
+  }
+   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Vitals'));
+    return Builder(
+      builder: (newContext) {
+        return ElevatedButton.icon(
+          icon: const Icon(Icons.restart_alt, color: Colors.white),
+          label: const Text(
+            'Reset SharedPreferences',
+            style: TextStyle(color: Colors.white),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.redAccent,
+            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          onPressed: () => _resetPrefs(newContext),
+        );
+      },
+    );
   }
 }
